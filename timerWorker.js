1ced0c03158a1605c09814b2f9458c47f38a4d38
Shaun Fox <shaun@wheelhouse-studio.com>
2018-03-08 12:05:06 -0500

    Updates

--- timerWorker.js
+++ timerWorker.js
@@ -2,7 +2,7 @@ let count = 0;
 
 function timerTick() {
   count += 1;
-  postMessage(count, 'https://foxshaunr.github.io/');
+  postMessage(count, 'https://foxshaunr.github.io');
   setTimeout(timerTick, 1000);
 }
 
